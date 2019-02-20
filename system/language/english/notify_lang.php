ca5c2d14340489576d6e193f2d12ee04108f25ec
Harun Šaranović <32799261+harunsaranovic@users.noreply.github.com>
2019-02-20 08:00:12 +0100

    Add files via upload

---
+++ system/language/english/notify_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['notify_view_subject'] = 'Profile View';
+$lang['notify_view_message'] = '%s viewed your profile.';
+$lang['notify_view_push'] = '%s viewed your profile.';
+
+$lang['notify_love_subject'] = 'New LOVE!';
+$lang['notify_love_message'] = '%s sent you a LOVE!';
+$lang['notify_love_push'] = '%s sent you a LOVE!';
+
+$lang['notify_request_subject'] = 'Friendship Request';
+$lang['notify_request_message'] = '%s wants to be friends with you.';
+$lang['notify_request_push'] = '%s wants to be friends with you.';
+
+$lang['notify_accept_subject'] = 'Friendship Request Accepted';
+$lang['notify_accept_message'] = '%s accepted your friendship request.';
+$lang['notify_accept_push'] = '%s accepted your friendship request.';
+
+$lang['notify_pm_subject'] = 'New private message';
+$lang['notify_pm_message'] = 'You have received some new private messages.';
+$lang['notify_pm_push'] = '%s has sent you a private message.';
